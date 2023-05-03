5eda08ea4b6585d04279d0952006eae63e8d7896
S4ntiego <adamksiazek123@gmail.com>
2023-05-03 16:06:37 +0200

    escaped apos

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -53,7 +53,7 @@ function Main() {
           <div className="row">
             <div className="col-1-of-2">
               <h3 className="heading-tertiary u-margin-bottom-small">
-                You're going to fall in love with nature
+                You&apos;re going to fall in love with nature
               </h3>
               <p className="paragraph">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -488,9 +488,9 @@ function Main() {
             </h3>
             <p className="popup__text">
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
+              industry. Lorem Ipsum has been the industry&apos;s standard dummy
+              text ever since the 1500s, when an unknown printer took a galley
+              of type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
               electronic typesetting, remaining essentially unchanged. It was
               popularised in the 1960s with the release of Letraset sheets
